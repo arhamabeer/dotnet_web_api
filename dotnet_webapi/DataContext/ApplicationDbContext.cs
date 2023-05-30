63bc6051451a731832a7b71754d095e5e8e27a97
arhamabeer <arhamabeerahmed@gmail.com>
2023-05-30 18:39:51 +0500

    ofc/30-4

---
+++ dotnet_webapi/DataContext/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+namespace dotnet_webapi.DataContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {   }
+
+        public DbSet<Employee> Branches { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
--- dotnet_webapi/Program.cs
+++ dotnet_webapi/Program.cs
@@ -1,8 +1,11 @@
+using dotnet_webapi.DataContext;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
