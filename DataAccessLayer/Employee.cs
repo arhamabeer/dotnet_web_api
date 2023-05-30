52f29012e7b64db476c28ee50a0c2de90130b621
arhamabeer <arhamabeerahmed@gmail.com>
2023-05-30 17:31:05 +0500

    first commit

---
+++ DataAccessLayer/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccessLayer
+{
+    public class Employee
+    {
+        public string Name{ get; set; }
+        public int Id { get; set; }
+        public string City { get; set; }
+    }
+}
