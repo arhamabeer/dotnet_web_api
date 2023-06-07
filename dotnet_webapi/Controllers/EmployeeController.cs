487e79cba83f28f9554d7c27786a28c44b971717
arhamabeer <arhamabeerahmed@gmail.com>
2023-06-07 18:06:16 +0500

    ofc/7-6

--- dotnet_webapi/Controllers/EmployeeController.cs
+++ dotnet_webapi/Controllers/EmployeeController.cs
@@ -28,5 +28,96 @@ namespace dotnet_webapi.Controllers
                 });
             }
         }
+
+        [HttpGet("{id=int}")]
+        public async Task<ActionResult<Employee>> GetEmployee(int id)
+        {
+            try
+            {
+                var result = await _employeeRepository.GetEmployee(id);
+
+                if(result == null)
+                {
+                    return NotFound();
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(501, new
+                {
+                    originalError = ex.Message,
+                    errorMessage = "Failed to fetch Data from the Database."
+                });
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Employee>> AddEmployee(Employee employee)
+        {
+            try
+            {
+                var newEmployee = await _employeeRepository.AddEmployee(employee);
+                return Ok(newEmployee);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(501, new
+                {
+                    originalError = ex.Message,
+                    errorMessage = "Failed to save data to the Database."
+                });
+            }
+        }
+        [HttpPut("{id=int}")]
+        public async Task<ActionResult<Employee>> UpdateEmployee(int id, Employee employee)
+        {
+            try
+            {
+               if(id != employee.Id)
+                {
+                    return BadRequest("Id Mismatched!");
+                }
+                
+                var fetchedEmployee = await _employeeRepository.GetEmployee(id);
+                if(fetchedEmployee == null)
+                {
+                    return NotFound($"Cannot find any Employee with id {id}");
+                }
+
+                var result = await _employeeRepository.UpdateEmployee(employee);
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(501, new
+                {
+                    originalError = ex.Message,
+                    errorMessage = "Failed to update data from the Database."
+                });
+            }
+        }
+        [HttpDelete("{id=int}")]
+        public async Task<ActionResult> DeleteEmployee(int id)
+        {
+            try
+            {
+                var fetchedEmployee = await _employeeRepository.GetEmployee(id);
+                if (fetchedEmployee == null)
+                {
+                    return NotFound($"Cannot find any Employee with id {id}");
+                }
+                 _employeeRepository.DeleteEmployee(id);
+                return Ok("Employee Deleted.");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(501, new
+                {
+                    originalError = ex.Message,
+                    errorMessage = "Failed to update data from the Database."
+                });
+            }
+        }
     }
 }
